688cc5908d614fb7e4b91284f2212646834a0d87
luochuanyuewu <luochuanyuewu@gmail.com>
2023-05-17 13:08:28 +0800

    chore: update

--- components/homepage/BlogLinks.tsx
+++ components/homepage/BlogLinks.tsx
@@ -8,13 +8,13 @@ export function BlogLinks() {
       <Link href="/projects" className="hover:underline">
         <Twemoji emoji="hammer-and-wrench" />
         <span data-umami-event="home-link-projects" className="ml-1.5">
-          What have I built?
+          我创作了什么？
         </span>
       </Link>
       <Link href="/blog" className="hover:underline">
         <Twemoji emoji="memo" />
         <span data-umami-event="home-link-blog" className="ml-1.5">
-          My writings
+          我的作品
         </span>
       </Link>
       <Link href="/snippets" className="hover:underline">
@@ -26,13 +26,13 @@ export function BlogLinks() {
       <Link href="/about" className="hover:underline">
         <Twemoji emoji="face-with-monocle" />
         <span data-umami-event="home-link-about" className="ml-1.5">
-          More about me and myself
+          更多关于我和我自己的信息
         </span>
       </Link>
       <Link href="/resume" className="hover:underline">
         <Twemoji emoji="briefcase" />
         <span data-umami-event="home-link-resume" className="ml-1.5">
-          My career
+          职业生涯
         </span>
       </Link>
       <Link href={siteMetadata.analyticsURL} className="hover:underline">
--- components/homepage/Greeting.tsx
+++ components/homepage/Greeting.tsx
@@ -7,7 +7,7 @@ export function Greeting() {
   )
   return (
     <div className={className}>
-      Howdy, fellow! <i className="twa twa-waving-hand"></i>
+      你好，伙计! <i className="twa twa-waving-hand"></i>
     </div>
   )
 }
